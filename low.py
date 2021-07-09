7ced2697298126b8a803ea7a5e8d732c748f7503
unknown <trinhtrungkientb99@gmail.com>
2021-07-09 08:46:40 +0700

    update low level

--- low.py
+++ low.py
@@ -5,6 +5,13 @@ graph = Map("Instances/20.10.1.txt")
 
 t_route = [18, 14, 6, 8, 1, 5, 16, 4, 15, 11, 3, 7, 9, 17, 12, 13, 10, 19, 2]
 
-solver(graph, t_route)
+fitness, max_cost, best_u_tour = solver(graph, t_route)  
+
+#fitness = 1 - (cost khi có drone hỗ trợ / max_cost)
+#max_cost: Tổng thời gian chờ của mẫu ứng với hành trình t_route khi không có drone hỗ trợ 
+#best_u_tour: hành trình của uav 
+
+print(fitness, max_cost, best_u_tour)
+
 
 #plt.show()
